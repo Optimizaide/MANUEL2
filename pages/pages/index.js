eede72bdbc64fec1d9f5e8f8f5691fd9f278ed4c
Optimizaide <florent_seguy7@hotmail.com>
2025-07-29 16:35:36 +0200

    Update index.js

--- pages/pages/index.js
+++ pages/pages/index.js
@@ -1,1 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Bienvenue dans l'app de coaching</h1>
+      <p>Accède à ta page de programme via : <code>/client/nomduclient</code></p>
+    </div>
+  );
+}
 
